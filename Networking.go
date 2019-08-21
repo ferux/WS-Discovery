fabce88546c599f545a46131037a37dda8fdba27
Aleksandr Trushkin <trushkin@restream.rt.ru>
2019-08-21 12:23:39 +0300

    use google.uuid

--- Networking.go
+++ Networking.go
@@ -11,28 +11,29 @@ package WS_Discovery
 
 import (
 	"fmt"
-	"net"
-	"golang.org/x/net/ipv4"
-	"time"
 	"log"
-	"github.com/satori/go.uuid"
+	"net"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/net/ipv4"
 )
 
-const bufSize  = 8192
+const bufSize = 8192
 
-func SendProbe(interfaceName string, scopes, types []string, namespaces map[string]string) []string{
+func SendProbe(interfaceName string, scopes, types []string, namespaces map[string]string) []string {
 	// Creating UUID Version 4
-	uuidV4 := uuid.Must(uuid.NewV4())
+	uuidV4 := uuid.New().String()
 	//fmt.Printf("UUIDv4: %s\n", uuidV4)
 
-	probeSOAP := buildProbeMessage(uuidV4.String(), scopes, types, namespaces)
+	probeSOAP := buildProbeMessage(uuidV4, scopes, types, namespaces)
 	//probeSOAP = `<?xml version="1.0" encoding="UTF-8"?><Envelope xmlns="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing"><Header><a:Action mustUnderstand="1">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</a:Action><a:MessageID>uuid:78a2ed98-bc1f-4b08-9668-094fcba81e35</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><a:To mustUnderstand="1">urn:schemas-xmlsoap-org:ws:2005:04:discovery</a:To></Header><Body><Probe xmlns="http://schemas.xmlsoap.org/ws/2005/04/discovery"><d:Types xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery" xmlns:dp0="http://www.onvif.org/ver10/network/wsdl">dp0:NetworkVideoTransmitter</d:Types></Probe></Body></Envelope>`
 
 	return sendUDPMulticast(probeSOAP.String(), interfaceName)
 
 }
 
-func sendUDPMulticast (msg string, interfaceName string) []string {
+func sendUDPMulticast(msg string, interfaceName string) []string {
 	var result []string
 	data := []byte(msg)
 	iface, err := net.InterfaceByName(interfaceName)
@@ -57,7 +58,7 @@ func sendUDPMulticast (msg string, interfaceName string) []string {
 		if err := p.SetMulticastInterface(ifi); err != nil {
 			fmt.Println(err)
 		}
-		p.SetMulticastTTL(2)
+		_ = p.SetMulticastTTL(2)
 		if _, err := p.WriteTo(data, nil, dst); err != nil {
 			fmt.Println(err)
 		}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/ferux/WS-Discovery
+
+go 1.12
+
+require (
+	github.com/beevik/etree v1.1.0
+	github.com/elgs/gostrgen v0.0.0-20161222160715-9d61ae07eeae // indirect
+	github.com/google/uuid v1.1.1
+	github.com/yakovlevdmv/gosoap v0.0.0-20180512142237-299a954b1c6d
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7
+)
